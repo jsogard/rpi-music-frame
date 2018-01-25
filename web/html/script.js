731f62f0328eeecb91c7435cd770f91742c97179
Joe Sogard <jmsogard@gmail.com>
2018-01-25 17:42:30 -0600

    add different content functionality; change icons

--- web/html/script.js
+++ web/html/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
 	
+	$(".util > img").hide();
+	
+	$(".util").hide();
+	
+	$(".util").each(function(index, element){
+			var imgSrc = $(element).find("img").attr("src");
+			$("#navbar table tr").prepend(
+				$("<td>\
+					<a>\
+						<img src='" + imgSrc +"'>\
+						<div><p>" + $(element).attr("id") + "</p></div>\
+					</a>\
+				</td>")
+			);
+		});
+		
+		$("#navbar a")
+			.mouseenter(function(){
+				var p = $(this).find("p")[0];
+				$(p).fadeIn();
+			})
+			.mouseleave(function(){
+				var p = $(this).find("p")[0];
+				$(p).fadeOut();
+			})
+			.click(function(){
+				var p = $(this).find("p")[0];
+				var id = $(p).text();
+				$("#content > div").each(function(index, element){
+					if($(element).attr('id') == id)
+						$(element).fadeIn();
+					else
+						$(element).hide();
+				})
+			})
 });
