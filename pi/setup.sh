2f073141c502ef0ca58b235a306074a93b25f770
Joe Sogard <jmsogard@gmail.com>
2018-01-24 21:29:26 -0600

    password, wifi, ssh config

---
+++ pi/setup.sh
@@ -0,0 +1,11 @@
+passwd
+# set password
+sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+# network={
+# 	ssid="<wifi>"
+# 	psk="<pwd>"
+# }
+hostname -I
+# record ip
+sudo raspi-config
+# Interfacing Options > SSH
