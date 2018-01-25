357280cc51f8fbcfd662931c084078ba9b41c35c
Joe Sogard <jmsogard@gmail.com>
2018-01-24 23:18:42 -0600

    video setup working

---
+++ pi/video_setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y install omxplayer
+sudo chmod 777 /dev/vchiq
+sudo chmod -R 755 /home/pi/Videos
+# omxplayer -o hdmi <file>
--- pi/webapp_setup.sh
+++ pi/webapp_setup.sh
@@ -3,5 +3,6 @@ sudo apt-get upgrade
 sudo apt-get install apache2 mysql-server libapache2-mod-auth-mysql php
 sudo mysql_install_db
 sudo service apache2 restart
+sudo chown -R pi /var/www
 echo 'Site now visible at $(hostname -I)'
 echo 'Put files into /var/www/'
